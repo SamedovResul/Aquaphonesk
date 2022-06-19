a7a6c2d9c56de83897d229100a9345998a6a3069
SamedovRasul <samedovrasul7@gmail.com>
2022-06-19 16:09:06 +0400

    first commit

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ app.use(cors());
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
 
-app.get('/test', (req,res) =>{
-  res.send("hello world")
-})
+
 
 app.use('/',temprature)
 
